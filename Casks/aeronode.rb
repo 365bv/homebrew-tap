6ffaa33c123bd5238efdbe805a54595730756272
Vitalii Bazavluk <bv365@icloud.com>
2026-05-22 04:26:23 +0300

    Refactor: typo removed

--- Casks/aeronode.rb
+++ Casks/aeronode.rb
@@ -3,7 +3,7 @@ cask "aeronode" do
   
   sha256 "a246854128849975dca9bdc1833511a7e719114ea17abc3645a88c5009e91316"
 
-  # Переконайся, що посилання веде саме на твій репозиторій з macOS застосунком
+
   url "https://github.com/365bv/AeroNode-macOS/releases/download/v1.1.0/AeroNode.dmg"
   name "AeroNode"
   desc "IoT Monitoring Control Center for Wind Turbines"
@@ -11,12 +11,7 @@ cask "aeronode" do
 
   app "AeroNode.app"
 
-  postflight do
-    system_command "xattr",
-                   args: ["-cr", "#{appdir}/AeroNode.app"],
-                   sudo: false
-  end
-
+  
   zap trash: [
     "~/Library/Application Support/com.vitaliibazavluk.AeroNode",
     "~/Library/Preferences/com.vitaliibazavluk.AeroNode.plist",

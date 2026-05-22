3d60008f4227ab1b51c9def58087008502775933
Vitalii Bazavluk <bv365@icloud.com>
2026-05-22 04:21:24 +0300

    Update AeroNode to v1.1.0

--- Casks/aeronode.rb
+++ Casks/aeronode.rb
@@ -1,10 +1,10 @@
 cask "aeronode" do
-  version "1.0.0"
+  version "1.1.0"
   
-  sha256 "46bdce5927efed0d096f601c4443a50b7e2adaf6360064d730af4a5b79f5e8bd"
+  sha256 "a246854128849975dca9bdc1833511a7e719114ea17abc3645a88c5009e91316"
 
   # Переконайся, що посилання веде саме на твій репозиторій з macOS застосунком
-  url "https://github.com/365bv/AeroNode-macOS/releases/download/v1.0.0/AeroNode.dmg"
+  url "https://github.com/365bv/AeroNode-macOS/releases/download/v1.1.0/AeroNode.dmg"
   name "AeroNode"
   desc "IoT Monitoring Control Center for Wind Turbines"
   homepage "https://github.com/365bv/AeroNode-macOS"
